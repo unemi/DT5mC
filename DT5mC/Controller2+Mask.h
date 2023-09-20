91c20c96c9af5e53d3ad6193052e8939e7257d3f
Tatsuo Unemi <unemi@iss.soka.ac.jp>
2023-09-20 21:09:34 +0900

    Add masking bitmap edit.

---
+++ DT5mC/Controller2+Mask.h
@@ -0,0 +1,16 @@
+//
+//  Controller2+Mask.h
+//  DT5mC
+//
+//  Created by Tatsuo Unemi on 2023/09/19.
+//
+
+#import "Controller2.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Controller2 ()
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DT5mC/Controller2+Mask.m
@@ -0,0 +1,12 @@
+//
+//  Controller2+Mask.m
+//  DT5mC
+//
+//  Created by Tatsuo Unemi on 2023/09/19.
+//
+
+#import "Controller2+Mask.h"
+
+@implementation Controller2 (MaskEdit)
+
+@end
