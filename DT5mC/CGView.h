93493cc60704bc49a336c5f2f1648008cc21a391
Tatsuo Unemi <unemi@iss.soka.ac.jp>
2024-02-21 19:20:50 +0900

    Added functions to copy the image to pasteboard, and optional code for statistic analysis.

---
+++ DT5mC/CGView.h
@@ -0,0 +1,16 @@
+//
+//  CGView.h
+//  DT5mC
+//
+//  Created by Tatsuo Unemi on 2024/02/20.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CGView : NSView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DT5mC/CGView.m
@@ -0,0 +1,18 @@
+//
+//  CGView.m
+//  DT5mC
+//
+//  Created by Tatsuo Unemi on 2024/02/20.
+//
+
+#import "CGView.h"
+
+@implementation CGView
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end
